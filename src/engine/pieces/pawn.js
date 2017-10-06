6b0ce19a0b95334b44e3ff18b4e9804622b8dba7
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 12:39:53 +0100

    Made pawns move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,10 @@
 import Piece from './piece';
+import board from '../board.js';
+import findPiece from '../board.js';
+
+
+import Square from "../square";
+import Player from "../player";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +12,13 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
-    }
-}
+        let pawnPosition = board.findPiece(this);
+        let moves = new Array(0);
+        console.log(Player.WHITE);
+        if (this.player === Player.WHITE ) {
+            moves.push(Square.at(pawnPosition.row +1, pawnPosition.col)); }
+        else  {
+            moves.push(Square.at(pawnPosition.row -1, pawnPosition.col));
+        }
+        return moves;
+}}
---
+++ tests/engine/pieces/pawnmove.js
@@ -0,0 +1,6 @@
+import 'chai/register-should';
+import Board from '../../src/engine/board';
+import Pawn from '../../src/engine/pieces/pawn';
+import Player from '../../src/engine/player';
+import Square from '../../src/engine/square';
+
